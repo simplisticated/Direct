d275f57999d41d4b22848897467a5193f2aa7be3
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-01-07 23:25:51 +0300

    Updated demo project

--- Demo/DirectDemo/DirectDemo/AppDelegate.swift
+++ Demo/DirectDemo/DirectDemo/AppDelegate.swift
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         DemoNavigator.shared
             .createWindow()
-            .setScene(scene: .main)
+            .setScene(.main)
         
         return true
     }
