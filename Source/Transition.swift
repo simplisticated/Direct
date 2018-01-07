7726104efed57e89ae3446709538b1040c9e8663
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-01-07 19:10:17 +0300

    Updated Transition enumeration

--- Source/Transition.swift
+++ Source/Transition.swift
@@ -11,9 +11,9 @@ import UIKit
 
 public enum Transition {
     case push(viewController: UIViewController, animated: Bool)
-    case replaceTop(replacement: UIViewController, animated: Bool)
     case pop(animated: Bool)
     case popToRootViewController(animated: Bool)
     case present(viewController: UIViewController, animated: Bool, completion: (() -> Void)?)
     case dismiss(animated: Bool, completion: (() -> Void)?)
+    case replaceTop(replacement: UIViewController, animated: Bool)
 }
