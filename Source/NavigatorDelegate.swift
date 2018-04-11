5aeb124cedf0525a763b8af206c1bd17436f79c9
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-04-11 11:20:12 +0300

    Added NavigatorDelegate protocol

---
+++ Source/NavigatorDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  NavigatorDelegate.swift
+//  Direct
+//
+//  Created by Igor Matyushkin on 11.04.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public protocol NavigatorDelegate: class {
+    
+    func navigator(_ navigator: Navigator, willCreateWindowOfType type: UIWindow.Type)
+    
+    func navigator(_ navigator: Navigator, didCreateWindow window: UIWindow)
+    
+    func navigator(_ navigator: Navigator, willChangeSceneTo newScene: Scene)
+    
+    func navigator(_ navigator: Navigator, didChangeSceneTo newScene: Scene)
+    
+    func navigator(_ navigator: Navigator, willPerformTransition transition: Transition)
+    
+    func navigator(_ navigator: Navigator, didPerformTransition transition: Transition)
+    
+}
